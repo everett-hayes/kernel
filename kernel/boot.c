47251361244c7848b4147cb63ed7cca5b340fe49
Attari <attarira@wilkinson.cs.grinnell.edu>
2022-02-02 19:05:52 -0600

    boyyyyy

--- kernel/boot.c
+++ kernel/boot.c
@@ -1,5 +1,6 @@
 #include <stdint.h>
 #include <stddef.h>
+#include <stdarg.h>
 
 #include "stivale2.h"
 #include "util.h"
@@ -147,7 +148,6 @@ void kprint_num(uint64_t value, int base) {
 void kprint_d(uint64_t value) {
   kprint_num(value, DECIMAL);
 }
-
 // Print an unsigned 64-bit integer value to the terminal in lowercase hexadecimal notation (no leading zeros or “0x” please!)
 void kprint_x(uint64_t value) {
   kprint_num(value, HEXADECIMAL);
@@ -159,19 +159,82 @@ void kprint_p(void* ptr) {
   kprint_x((uint64_t) ptr);
 }
 
+void kprint_f(const char* format, ...) {
+  const char* pos = format;
+  va_list ap;
+
+  va_start(ap, format);
+
+  while (*pos != '\0') {
+
+    if (*pos == '%') {
+      pos++;
+
+      switch (*pos) {
+        case 'c':
+          kprint_c(va_arg(ap, int));
+          break;
+        case 's':
+          kprint_s(va_arg(ap, const char*));
+          break;
+        case 'd':
+          kprint_d(va_arg(ap, uint64_t));
+          break;
+        case 'x':
+          kprint_x(va_arg(ap, uint64_t));
+          break;
+        case 'p':
+          kprint_p(va_arg(ap, void*));
+          break;
+        case '%':
+          kprint_c('%');
+          break;
+        default:
+          kprint_c('?');
+      }
+    } else {
+      kprint_c(*pos);
+    }
+
+    pos++;
+  }
+
+  va_end(ap);
+}
+
+void findMemory(struct stivale2_struct* hdr) {
+
+
+  //virtual memory
+  struct stivale2_struct_tag_hhdm* virtual_tag = find_tag(hdr, STIVALE2_STRUCT_TAG_HHDM_ID);
+  uint64_t virtualMemStart = virtual_tag->addr;
+
+  // physical memory
+  struct stivale2_struct_tag_memmap* physical_tag = find_tag(hdr, STIVALE2_STRUCT_TAG_MEMMAP_ID);
+
+  kprint_f("Usable Memory:\n");
+  for (int i = 0; i < physical_tag->entries; i++) {
+    // if the memory is usable
+    if (physical_tag->memmap[i].type == 1) {
+      uint64_t start = physical_tag->memmap[i].base;
+      uint64_t end = physical_tag->memmap[i].base + physical_tag->memmap[i].length;
+      uint64_t virtualStart = start + virtualMemStart;
+      uint64_t virtualEnd = end + virtualMemStart;
+      kprint_f("\t0x%x-0x%x mapped at 0x%x-0x%x\n", start, end, virtualStart, virtualEnd);
+    }
+  }
+}
+
 void _start(struct stivale2_struct* hdr) {
   // We've booted! Let's start processing tags passed to use from the bootloader
   term_setup(hdr);
 
-  // Print a greeting
-  term_write("Hello Kernel!\n", 14);
-
+  findMemory(hdr);
+  kprint_s("\n");
   // Test print functions
   // kprint_c('f');
   // kprint_s("Hello World");
-  kprint_d(0);
-
-  kprint_d(12345678);
+  kprint_f("Hello world it is %s. I am %d years old", "Everett", 12);
   // int test = 10;
   // kprint_p(&test);
 
